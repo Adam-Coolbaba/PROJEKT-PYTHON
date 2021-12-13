6e777618398fa1449590ba3e37e06f2c7922c7ed
Michał Szczepanik <trediraz@gmail.com>
2021-12-13 20:54:16 +0100

    Change main file name and function names in dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -1,21 +1,21 @@
 import numpy as np
 
 
-def calculate_kinetic_energy(m, v):
+def calc_kinetic_energy(m, v):
     return m*v**2/2
 
 # w = czestosc wlasna, jesli masz jakis lepszy pomysl na oznaczenie to mozesz zmienic
-def calculate_potential_energy(m, w, x):
+def calc_potential_energy(m, w, x):
     return m*(w*x)**2/2
 
-def calculate_deviation (fi0 ,w , t):
+def calc_deviation (fi0, w, t):
     return fi0*np.cos(w*t)
 
-def calculate_osc_speed(t, w, a):
+def calc_osc_speed(t, w, a):
     return a*w*np.cos(w*t)
 
 
-def calculate_osc_acceleration_magnitude(t, w, a):
+def calc_osc_acceleration_magnitude(t, w, a):
     return -a*w**2*np.sin(w*t)
 
 #jesli masz jakis pomysl na inne parametry ktore warto by umiescic w tabeli powiazanej z amimacja to smialo dodawaj
--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import dynamics
 
 def test_module():
     # Funkcja tymczasowa do usuniecia potem
-    print(dynamics.calculate_kinetic_energy(3, 3))
-    print(dynamics.calculate_potential_energy(1, 5, 3))
-    print(dynamics.calculate_osc_speed(0.5, 3.14, 10))
-    print(dynamics.calculate_osc_acceleration_magnitude(0.5,3.14,10))
+    print(dynamics.calc_kinetic_energy(3, 3))
+    print(dynamics.calc_potential_energy(1, 5, 3))
+    print(dynamics.calc_osc_speed(0.5, 3.14, 10))
+    print(dynamics.calc_osc_acceleration_magnitude(0.5, 3.14, 10))
 
 test_module()
 
 #Inicjalizacja czestowsci wlasnych i amplitud
 w_x = 2
-w_y = 2
+w_y = 3
 a_x = 1
 a_y = 1
 f = np.pi/2
