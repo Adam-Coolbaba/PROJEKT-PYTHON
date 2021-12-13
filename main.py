87daa61d7914114eb37a15dfae46d31f5b16cada
Michał Szczepanik <trediraz@gmail.com>
2021-12-13 22:03:01 +0100

    Add graph UI and basic user interaction

--- main.py
+++ main.py
@@ -1,29 +1,86 @@
-# This is a sample Python script.
+import tkinter
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 import dynamics
 
 
-def test_module():
-    # Funkcja tymczasowa do usuniecia potem
-    print(dynamics.calc_kinetic_energy(3, 3))
-    print(dynamics.calc_potential_energy(1, 5, 3))
-    print(dynamics.calc_osc_speed(0.5, 3.14, 10))
-    print(dynamics.calc_osc_acceleration_magnitude(0.5, 3.14, 10))
+def update_graph(event):
+    #Do usuniecia potem, ale narazie sie przydaje
+    if str(event.key) == 'q':
+        _quit()
+    #----------
+    try:
+        w_y = int(event.key)
+        line.set_ydata(np.sin(int(event.key) * t))
+    except ValueError:
+        w_y = 1
+        line.set_ydata(np.sin(1 * t))
+    fig.suptitle(f'Wykres dla: w1 = {w_x}, w2 = {w_y}')
+    fig.canvas.draw()
+    mpl.backend_bases.key_press_handler(event, canvas)
 
-test_module()
 
-#Inicjalizacja czestowsci wlasnych i amplitud
+def set_up_canvas(c):
+    c.draw()
+    c.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+    c.mpl_connect("key_press_event", update_graph)
+
+
+def _quit():
+    root.quit()
+    root.destroy()
+
+
+GRAPH_SIZE = 6
+FONT_SIZE = 15
+root = tkinter.Tk()
+root.wm_title("Wykres drgan prostopadlych")
+
 w_x = 2
-w_y = 3
+w_y = 2
 a_x = 1
 a_y = 1
 f = np.pi/2
 
-#trajektoria w postaci paramatrycznej
-t = np.arange(0,10,0.01)
+t = np.arange(0, 10, 0.01)
 x = a_x*np.sin(w_x*t)
 y = a_y*np.sin(w_y*t + f)
 
-plt.plot(x,y,'g-')
-plt.show()
+fig = Figure(figsize=(GRAPH_SIZE, GRAPH_SIZE), dpi=100)
+ax = fig.add_subplot(111)
+line, = ax.plot(x, y, 'g-')
+canvas = FigureCanvasTkAgg(fig, master=root)
+fig.supxlabel("x [m]", fontsize=FONT_SIZE)
+fig.supylabel("y [m]", fontsize=FONT_SIZE)
+fig.suptitle("Wykres", fontsize=FONT_SIZE, fontweight=500)
+set_up_canvas(canvas)
+
+button = tkinter.Button(master=root, text="Zamknij", command=_quit)
+button.pack(side=tkinter.BOTTOM)
+
+tkinter.mainloop()
+
+
+
+
+
+
+#Propozycja jak będziemy mieli za dużo czasu:
+def Td():
+    ax = plt.axes(projection = '3d')
+
+    t= np.arange(0,10,0.01)
+    x = np.sin(2*t)
+    y = np.sin(4*t)
+    z = np.sin(6*t)
+
+    ax.plot3D(x,y,z,'g-')
+    plt.show()
+
+#Td()
+
