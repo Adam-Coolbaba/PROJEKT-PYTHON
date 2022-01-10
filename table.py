ee46dc3fcb436a9246ac50e094ef6168b427b72f
Michał Szczepanik <trediraz@gmail.com>
2022-01-10 23:40:18 +0100

    Change radius to curvature

--- table.py
+++ table.py
@@ -19,12 +19,7 @@ class Table:
 
     def update(self, updated_list):
         for i in range(len(self.value_label_list)):
-            text = ''
-            if isinstance(updated_list[i],float):
-                text = '{:.2f}'.format(updated_list[i])
-            else:
-                text = updated_list[i]
-            self.value_label_list[i].config(text=text)
+            self.value_label_list[i].config(text='{:.2f}'.format(updated_list[i]))
 
 lst = [("x [m]", "0"),
         ("y [m]", "0"),
@@ -32,7 +27,7 @@ lst = [("x [m]", "0"),
         ("a [m/s^2]", "0"),
         ("Ek [J]", "0"),
         ("Ep [J]", "0"),
-        ("R [m]" , "0")]
+        ("\u03BA [m]" , "0")]
 
 total_rows = len(lst)
 total_columns = len(lst[0])
